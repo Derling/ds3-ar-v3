c8cf306ac6a025dad3001b0a077c076018323741
Derling <derling0704@gmail.com>
2018-06-05 12:20:06 -0400

    Moved infusions drop down menu to its own component

---
+++ src/components/SelectedWeapon/Infusions/infusions.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+
+class Infusions extends Component {
+	constructor(props){
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.props.selected(event.target.value);
+	}
+
+	render() {
+		console.log('creating infusions component');
+		return (
+			<select onChange={this.handleChange} value={this.props.value}>
+				<option value="Blessed">Blessed</option>
+				<option value="Blood">Blood</option>
+				<option value="Chaos">Chaos</option>
+				<option value="Crystal">Crystal</option>
+				<option value="Dark">Dark</option>
+				<option value="Deep">Deep</option>
+				<option value="Fire">Fire</option>
+				<option value="Heavy">Heavy</option>
+				<option value="Hollow">Hollow</option>
+				<option value="Lightning">Lightning</option>
+				<option value="Normal">Normal</option>
+				<option value="Poison">Poison</option>
+				<option value="Raw">Raw</option>
+				<option value="Refined">Refined</option>
+				<option value="Sharp">Sharp</option>
+				<option value="Simple">Simple</option>
+			</select>
+		)
+	}
+}
+
+export default Infusions;
